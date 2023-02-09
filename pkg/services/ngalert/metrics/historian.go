7137d0ae1125a6405a2f0952a5ef830c14b5bc9b
Alex Weaver <weaver.alex.d@gmail.com>
2023-02-09 12:30:02 -0600

    Measure TransitionsFailedTotal to complement TransitionsTotal

--- pkg/services/ngalert/metrics/historian.go
+++ pkg/services/ngalert/metrics/historian.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Historian struct {
-	TransitionsTotal      *prometheus.CounterVec
-	WritesTotal           prometheus.Counter
-	WritesFailedTotal     prometheus.Counter
-	ActiveWriteGoroutines prometheus.Gauge
-	PersistDuration       prometheus.Histogram
+	TransitionsTotal       *prometheus.CounterVec
+	TransitionsFailedTotal *prometheus.CounterVec
+	WritesTotal            prometheus.Counter
+	WritesFailedTotal      prometheus.Counter
+	ActiveWriteGoroutines  prometheus.Gauge
+	PersistDuration        prometheus.Histogram
 }
 
 func NewHistorianMetrics(r prometheus.Registerer) *Historian {
@@ -19,13 +20,19 @@ func NewHistorianMetrics(r prometheus.Registerer) *Historian {
 			Namespace: Namespace,
 			Subsystem: Subsystem,
 			Name:      "historian_transitions_total",
-			Help:      "The total number of state transitions recorded by the state historian.",
+			Help:      "The total number of state transitions processed by the state historian.",
+		}, []string{"org"}),
+		TransitionsFailedTotal: promauto.With(r).NewCounterVec(prometheus.CounterOpts{
+			Namespace: Namespace,
+			Subsystem: Subsystem,
+			Name:      "historian_transitions_failed_total",
+			Help:      "The total number of state transitions that failed to be written.",
 		}, []string{"org"}),
 		WritesTotal: promauto.With(r).NewCounter(prometheus.CounterOpts{
 			Namespace: Namespace,
 			Subsystem: Subsystem,
 			Name:      "historian_batch_writes_total",
-			Help:      "The total number of written state history batches.",
+			Help:      "The total number of state history batches that were attempted to be written.",
 		}),
 		WritesFailedTotal: promauto.With(r).NewCounter(prometheus.CounterOpts{
 			Namespace: Namespace,
